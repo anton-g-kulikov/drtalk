9f668e411b9395fc56c45e851a4a57d88fc11b70
Anton Kulikov <anton.g.kulikov@gmail.com>
2026-07-02 07:33:15 +0200

    feat(network): add dedicated full-page routes for inviting clinical partners

---
+++ src/app/dentist/network/invite/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { InviteClinicPageContent } from '@/components/prototype/InviteClinicPageContent';
+
+export default function DentistInvitePage() {
+  return <InviteClinicPageContent role="dentist" />;
+}
---
+++ src/app/network/invite/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { InviteClinicPageContent } from '@/components/prototype/InviteClinicPageContent';
+
+export default function SpecialistInvitePage() {
+  return <InviteClinicPageContent role="specialist" />;
+}
